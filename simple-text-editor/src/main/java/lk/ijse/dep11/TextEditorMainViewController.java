96ac1f33f6990add0d27389c7b6770ce9fabb670
DilankaSandeep <dilankacm@gmail.com>
2023-08-22 01:22:29 +0530

    User Guide Window Modal Completed

--- simple-text-editor/src/main/java/lk/ijse/dep11/TextEditorMainViewController.java
+++ simple-text-editor/src/main/java/lk/ijse/dep11/TextEditorMainViewController.java
@@ -1,11 +1,15 @@
 package lk.ijse.dep11;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.web.HTMLEditor;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class TextEditorMainViewController {
@@ -16,6 +20,9 @@ public class TextEditorMainViewController {
     public Rectangle dragBar;
     public  double x=0;
     public  double y=0;
+    public Button btnExit;
+    public MenuItem menuItemUserGuide;
+    public MenuItem menuitemAboutUs;
 
     public void menuItemNewOnAction(ActionEvent actionEvent) {
         htmlEditor.setHtmlText("");
@@ -34,5 +41,24 @@ public class TextEditorMainViewController {
     public void dragBarOnPressed(MouseEvent mouseEvent) {
         x=mouseEvent.getSceneX();
         y=mouseEvent.getSceneY();
+    }
+
+    public void btnExitOnAction(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+    public void menuItemUserGuideOnAction(ActionEvent actionEvent) throws Exception{
+        AnchorPane userGuideroot = FXMLLoader.load(getClass().getResource("/view/UserGuideView.fxml"));
+        Scene userGuideScene = new Scene(userGuideroot);
+        Stage userGuideStage = new Stage();
+        userGuideStage.initModality(Modality.WINDOW_MODAL);
+        userGuideStage.initOwner(root.getScene().getWindow());
+        userGuideStage.setScene(userGuideScene);
+        userGuideStage.setTitle("User Guide for Text Editor");
+        userGuideStage.centerOnScreen();
+        userGuideStage.show();
+    }
+
+    public void menuItemAboutUsOnAction(ActionEvent actionEvent) {
     }
 }
